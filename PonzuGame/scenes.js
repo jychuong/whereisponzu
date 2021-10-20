fcdfd759f17ec07d5265f0a0467841c3730c8368
jychuong <80948890+jychuong@users.noreply.github.com>
2021-10-20 12:42:53 -0700

    Add files via upload

---
+++ PonzuGame/scenes.js
@@ -0,0 +1,31 @@
+function menu() {
+  this.setup = function() {
+}
+  this.draw = function(){
+
+  }
+}
+
+function room1 (){
+  this.setup = function() {
+}
+  this.draw = function (){
+
+  }
+}
+
+function room2 (){
+  this.setup = function() {
+}
+  this.draw = function (){
+
+  }
+}
+
+function done (){
+  this.setup = function() {
+}
+  this.draw = function (){
+
+  }
+}
