171198c3995bfaffc80a434f81a62a662ba05ec9
jychuong <jessicaychuong@gmail.com>
2021-10-25 05:03:50 -0700

    .

--- scenes.js
+++ scenes.js
@@ -1,4 +1,34 @@
 
+function start(){
+  bgm.loop();
+  this.setup = function(){
+    clickS = new Clickable();
+      clickS.image = startBut;
+      clickS.fitImage = true;
+      clickS.imageScale = 1;
+      clickS.locate(65, 600);
+      clickS.resize(500,500);
+      clickS.onHover = function () {
+        clickS.imageScale = 1.05;
+        }
+      clickS.onOutside = function () {
+        clickS.imageScale = 1;
+      }
+       clickS.onRelease = function(){
+         mgr.showScene(room);
+         hover.play();
+       }
+  }
+
+  this.draw = function()
+      {
+         image(title,0,0,width,height);
+         clickS.draw();
+         image(ponzustart,0,0,width,height);
+
+
+      }
+}
 
 function room()  {
   this.setup = function(){
@@ -6,13 +36,10 @@ function room()  {
       click1.image = plantC;
       click1.fitImage = true;
       click1.imageScale = 1;
-      click1.text = "";
-      click1.textsize = 0;
-      click1.locate(20, 250);
-      click1.resize(400,500);
-      click1.strokeWeight = 0;
+      click1.locate(65, 235);
+      click1.resize(300,500);
       click1.onHover = function () {
-        click1.imageScale = 1.1;
+        click1.imageScale = 1.05;
         }
       click1.onOutside = function () {
         click1.imageScale = 1;
@@ -27,11 +54,11 @@ function room()  {
          click2.fitImage = true;
          click2.imageScale = 1;
          click2.text = "";
-         click2.locate(730, 150);
-         click2.resize(250,350);
+         click2.locate(735, 103);
+         click2.resize(260,360);
          click2.strokeWeight = 0;
          click2.onHover = function () {
-           click2.imageScale = 1.1;
+           click2.imageScale = 1.05;
            }
          click2.onOutside = function () {
            click2.imageScale = 1;
@@ -46,12 +73,12 @@ function room()  {
             click3.fitImage = true;
             click3.imageScale = 1;
             click3.text = "";
-            click3.locate(800, 150);
-            click3.resize(500,600);
+            click3.locate(800, 300);
+            click3.resize(500,380);
             click3.strokeWeight = 0;
             click3.color = "clear";
             click3.onHover = function () {
-              click3.imageScale = 1.1;
+              click3.imageScale = 1.02;
               }
             click3.onOutside = function () {
               click3.imageScale = 1;
@@ -60,6 +87,44 @@ function room()  {
                mgr.showScene(plush);
                hover.play();
              }
+
+             click7 = new Clickable();
+               click7.image = chairC;
+               click7.fitImage = true;
+               click7.imageScale = 1;
+               click7.locate(340, 180);
+               click7.resize(427,650);
+               click7.strokeWeight = 0;
+               click7.onHover = function () {
+                 click7.imageScale = 1.02;
+                 }
+               click7.onOutside = function () {
+                 click7.imageScale = 1;
+               }
+                click7.onRelease = function(){
+                  mgr.showScene(chair);
+                  hover.play();
+                }
+                click8 = new Clickable();
+                  click8.image = ballC;
+                  click8.fitImage = true;
+                  click8.imageScale = 1;
+                  click8.text = "";
+                  click8.locate(120, 680);
+                  click8.resize(280,380);
+                  click8.strokeWeight = 0;
+                  click8.color = "clear";
+                  click8.onHover = function () {
+                    click8.imageScale = 1.02;
+                    }
+                  click8.onOutside = function () {
+                    click8.imageScale = 1;
+                  }
+                   click8.onRelease = function(){
+                     mgr.showScene(ball);
+                     hover.play();
+                   }
+
   }
 
     this.enter = function()  {
@@ -68,14 +133,15 @@ function room()  {
 
 this.draw = function()
     {
-
        image(roomS,0,0,width,height);
        click1.draw();
        click2.draw();
+       image(bunk,0,0,width,height);
        click3.draw();
+       click7.draw();
+       click8.draw();
+
     }
-
-
 }
 
 ///////////////////////  2  ////////////////////////
@@ -86,8 +152,6 @@ function comp() {
       click6.image = back;
       click6.fitImage = true;
       click6.imageScale = 1;
-      click6.text = "";
-      click6.textsize = 0;
       click6.locate(10, 20);
       click6.resize(100,100);
       click6.strokeWeight = 0;
@@ -103,13 +167,10 @@ function comp() {
     }
   }
 
-    this.enter = function()  {
-
-    }
-
     this.draw = function() {
       image(compSc,0,0,width,height);
       click6.draw();
+      image(ponzufind,800,300,600,600);
     }
 }
 
@@ -122,8 +183,6 @@ this.setup = function() {
     click6.image = back;
     click6.fitImage = true;
     click6.imageScale = 1;
-    click6.text = "";
-    click6.textsize = 0;
     click6.locate(10, 20);
     click6.resize(100,100);
     click6.strokeWeight = 0;
@@ -138,11 +197,6 @@ this.setup = function() {
        hover.play();
   }
 }
-
-  this.enter = function()
-  {
-
-  }
 
     this.draw = function()
     {
@@ -161,11 +215,8 @@ function plush()  {
       click6.image = back;
       click6.fitImage = true;
       click6.imageScale = 1;
-      click6.text = "";
-      click6.textsize = 0;
       click6.locate(10, 20);
       click6.resize(100,100);
-      click6.strokeWeight = 0;
       click6.onHover = function () {
         click6.imageScale = 1.1;
         }
@@ -176,11 +227,6 @@ function plush()  {
          mgr.showScene(room);
          hover.play();
     }
-  }
-
-  this.enter = function()
-  {
-
   }
 
     this.draw = function()
@@ -196,23 +242,31 @@ function plush()  {
 function ball ()  {
 
 this.setup = function() {
-
+  click6 = new Clickable();
+    click6.image = back;
+    click6.fitImage = true;
+    click6.imageScale = 1;
+    click6.locate(10, 20);
+    click6.resize(100,100);
+    click6.strokeWeight = 0;
+    click6.onHover = function () {
+      click6.imageScale = 1.1;
+      }
+    click6.onOutside = function () {
+      click6.imageScale = 1;
+    }
+     click6.onRelease = function(){
+       mgr.showScene(room);
+       hover.play();
   }
-
-  this.enter = function()
-  {
-
   }
 
     this.draw = function()
     {
-
       image(ballSc,0,0,width,height);
+      click6.draw();
     }
-    this.mousePressed = function()
-    {
-        this.sceneManager.showNextScene();
-    }
+
 }
 
 /////////////////////////////////////5///////////////////
@@ -220,23 +274,31 @@ this.setup = function() {
 function chair ()  {
 
 this.setup = function() {
-
+  click6 = new Clickable();
+    click6.image = back;
+    click6.fitImage = true;
+    click6.imageScale = 1;
+    click6.locate(10, 20);
+    click6.resize(100,100);
+    click6.strokeWeight = 0;
+    click6.onHover = function () {
+      click6.imageScale = 1.1;
+      }
+    click6.onOutside = function () {
+      click6.imageScale = 1;
+    }
+     click6.onRelease = function(){
+       mgr.showScene(room);
+       hover.play();
   }
-
-  this.enter = function()
-  {
-
   }
 
     this.draw = function()
     {
-
       image(chairSc,0,0,width,height);
+      click6.draw();
     }
-    this.mousePressed = function()
-    {
-        this.sceneManager.showNextScene();
-    }
+
 }
 
 /////////////////////////////////////6///////////////////
--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
 var roomS, compSc, plantSc, plushSc, ballS, chairSc;
+var title;
 var hover;
 var click1, plantC;
 var click2, compC;
@@ -7,27 +8,47 @@ var click4, chairC;
 var click5, ballC;
 var click6, back;
 var click7, chairC;
+var click8, ballC;
+var startBut;
+var bunk;
+var bgm;
 var mgr;
 let canvas;
+var ponzu1,ponzu2;
+var ponzustart;
+var ponzufind;
+
 
 function preload() {
-
+  title = loadImage("assets/title.png");
+  startBut = loadImage("assets/startb.png");
   hover = loadSound("assets/hover.mp3");
+  bgm = loadSound("assets/bgm.mp3");
 
   back = loadImage("assets/arrow.png");
-
-  roomS = loadImage("assets/room.png");
+  bunk = loadImage("assets/bed.png");
+//different scenes
+  roomS = loadImage("assets/roomplain.png");
   compSc = loadImage("assets/compS.png");
   plantSc = loadImage("assets/plantS.png");
   plushSc = loadImage("assets/plushS.png");
   chairSc = loadImage("assets/chairS.png");
   ballSc = loadImage("assets/ballS.png");
   chairSc = loadImage("assets/chairS.png");
-
+//clickable buttons
   plantC = loadImage("assets/plant.png");
   compC = loadImage("assets/comp.png");
   plushC = loadImage("assets/plush.png");
   chairC = loadImage("assets/chair.png");
+  ballC = loadImage("assets/ball.png");
+
+//ponzu
+  ponzustart = loadImage("assets/ponzutitle.gif");
+  ponzufind = loadImage("assets/ponzuend.gif")
+
+
+
+
 }
 
 
@@ -36,6 +57,7 @@ function setup() {
     canvas.parent('sketch1');
     mgr = new SceneManager();
 
+    mgr.addScene(start);
     mgr.addScene (room);
     mgr.addScene (comp);
     mgr.addScene (plant);
